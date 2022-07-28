3a8dc1c30104279d4eb3e9a12aa0a27e92a34f57
eaydin <erkinresearch@gmail.com>
2022-07-28 16:10:21 +0300

    PostView.vue updated. Now it can be used to display all posts.

--- questapp-ui/src/components/PostView.vue
+++ questapp-ui/src/components/PostView.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    Post...
+  <div id="axios-get">
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }} | {{ post.text }}</li>
+    </ul>
+    <ul v-if="errors && errors.length">
+      <li v-for="(error, index) of errors" :key="index">
+        {{ index + 1 }} - {{ error.message }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+
+import axios from "axios"
+
 export default {
-  name: "PostView"
+  name: "PostView",
+  created() {
+    this.getAllPosts();
+  },
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+  methods: {
+    getAllPosts() {
+      axios
+          .get("http://localhost:8080/posts/list")
+          .then(response => (this.posts = response.data))
+          .catch(error => {
+            this.errors.push(error);
+          })
+    }
+  }
 }
 </script>
 
